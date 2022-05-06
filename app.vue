0d719cde883195ef8b569d5cf51496ff7c2f9c44
“shotam27“ <neshotam27@gmail.com>
2022-05-07 02:23:22 +0900

    first commit

---
+++ app.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const count = ref(0);
+const cards = ["cada", "cadb"];
+
+const increment = () => {
+  count.value++;
+};
+
+const decrement = () => {
+  console.log("in");
+  count.value--;
+};
+</script>
+
+<template>
+  <div class="game">
+    <div class="p-2 bg-gray-100 flex justify-around">
+      <div class="p-2 card" v-for="card in cards">{{ card }}</div>
+    </div>
+  </div>
+</template>
+
+<style>
+div {
+  border-radius: 10px;
+}
+.game {
+  width: 100%;
+}
+.card {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border: solid black;
+  background: gray;
+  color: white;
+  height: 140px;
+  width: 100px;
+  transform: scale(0.7);
+}
+</style>
